9be512088e75778863415e0f8f1f05d8735694e1
Aaron de Bruin <122994@student.drenthecollege.nl>
2022-01-20 14:23:31 +0100

    functie voor band aanmaken

--- EPK/resources/views/EPK/dashboard.blade.php
+++ EPK/resources/views/EPK/dashboard.blade.php
@@ -2,13 +2,22 @@
 
 @section('content')
 <div class="container">
+    @if (session()->has('message'))
+    <p> {{ session()->get('message') }}
+    @endif
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Maak hier je EPK</div>
                 
                 <div class="card-body">
-                   
+                    @if (Auth::check())
+
+                    <div class="">
+                        <a class="btn btn-primary" href="create">Maak band</a>
+                    </div>
+                        
+                    @endif
                 </div>
             </div>
         </div>
