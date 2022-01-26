7c789ef3c8be834ee89b93b1d3a34e6aa47a4e7c
elmedinarifi <elmedinarifi2000@gmail.com>
2022-01-26 12:23:21 +0100

    TXT

--- EPK/resources/views/index.blade.php
+++ EPK/resources/views/index.blade.php
@@ -24,9 +24,9 @@
         <img class="imgpic card-img-top" src="{{ asset('images/' . $post->image_path) }}" alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">{{ $post->title }}</h5>
-          <p class="card-text">{{ $post->description }}</p>
-          <strong> {{ $post->user->name }} </strong>
-           <p class="fst-italic">Aangemaakt op {{ date('jS M Y', strtotime($post->updated_at)) }}</p>
+          <p class="dcptn card-text fst-italic">{{ $post->description }}</p>
+          <strong class="text-decoration-underline" > {{ $post->user->name }} </strong>
+           <p class="angmkt">Aangemaakt op {{ date('jS M Y', strtotime($post->updated_at)) }}</p>
             {{-- Tijdelijke br's --}}
             <br>
             <br>
@@ -38,14 +38,9 @@
           @if (isset(Auth::user()->id) && Auth::user()->id ==$post->user_id)
 
           <p style="margin-left:90%;"> 
-            <a class="edtknp" href="/EPK/{{ $post->slug }}/edit">Edit</a> 
-
-          <p> 
-            <a href="/EPK/{{ $post->slug }}/edit" class="btn btn-primary" >
-              Edit 
-            </a> 
-
+            <a class="edtknp btn btn-primary" href="/EPK/{{ $post->slug }}/edit">Edit</a>
           </p>
+
           <p> 
             <form action="/EPK/{{ $post->slug }}" method="POST"> @csrf @method('delete')
               <button class="btn btn-outline-danger"> Delete  
