8aad17d41f92afdb9d92e1a85cadb2c01ca7e8c8
elmedinarifi <elmedinarifi2000@gmail.com>
2022-01-26 11:33:23 +0100

    BTN

--- EPK/resources/views/index.blade.php
+++ EPK/resources/views/index.blade.php
@@ -10,17 +10,17 @@
     @section('content')
     @foreach ($posts as $post)
     <div class="col-lg-4 col-md-4 col-sm-4 container justify-content-center">
-    <div class="card d-flex justify-content-center" style="width: 40rem;">
-        <img class="card-img-top" src="{{ asset('images/' . $post->image_path) }}" alt="Card image cap">
+    <div class="card d-flex justify-content-center" style="width: 150%;">
+        <img class="imgpic card-img-top" src="{{ asset('images/' . $post->image_path) }}" alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">{{ $post->title }}</h5>
           <p class="card-text">{{ $post->description }}</p>
           <strong> {{ $post->user->name }} </strong>
-           Aangemaakt op {{ date('jS M Y', strtotime($post->updated_at)) }}
+           <p>Aangemaakt op {{ date('jS M Y', strtotime($post->updated_at)) }}</p>
             {{-- Tijdelijke br's --}}
             <br>
             <br>
-          <a href="/EPK/{{ $post->slug }}" class="btn btn-primary">Lees meer</a>
+          <a href="/EPK/{{ $post->slug }}" class="btn btn-outline-primary">Lees meer</a>
 
           @if (isset(Auth::user()->id) && Auth::user()->id ==$post->user_id)
           <p> 
